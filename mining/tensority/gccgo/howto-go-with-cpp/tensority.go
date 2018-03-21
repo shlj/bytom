d4696a5f395846ba2b64770e503aceb0b7d4edda
HAOYUatHZ <haoyu@protonmail.com>
2018-03-22 07:41:29 +0800

    re-construct

---
+++ mining/tensority/gccgo/howto-go-with-cpp/tensority.go
@@ -0,0 +1,44 @@
+package main
+
+// #cgo LDFLAGS: -L . -lstdc++
+// #cgo CFLAGS: -I ./
+// #include "foo.h"
+import "C"
+
+import(
+	// "fmt"
+	"unsafe"
+)
+
+type GoFoo struct {
+	foo C.Foo
+}
+
+func New() GoFoo {
+	var ret GoFoo
+	ret.foo = C.FooInit()
+	return ret
+}
+
+func (f GoFoo) Free() {
+	C.FooFree(f.foo)
+}
+
+func (f GoFoo) Bar() {
+	C.FooBar(f.foo)
+}
+
+func testHelper(blockHeader [32]uint8, seed [32]uint8, hash [32]uint8) [32]uint8 {
+	a := (*[32]C.uchar)(unsafe.Pointer(&blockHeader[0]))
+	b := (*[32]C.uchar)(unsafe.Pointer(&seed[0]))
+	C.get(&a[0])
+	C.get(&b[0])
+
+	return hash
+}
+
+func main() {
+	foo := New()
+	foo.Bar()
+	foo.Free()
+}
