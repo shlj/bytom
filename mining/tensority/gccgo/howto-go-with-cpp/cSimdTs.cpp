3aab9c48e141a123e711b90907ed16f6710a32f3
HAOYUatHZ <haoyu@protonmail.com>
2018-03-24 16:33:53 +0800

    seedCache implementation

--- mining/tensority/gccgo/howto-go-with-cpp/cSimdTs.cpp
+++ mining/tensority/gccgo/howto-go-with-cpp/cSimdTs.cpp
@@ -7,15 +7,15 @@
 
 using namespace std;
 
-BytomMatList16 matList_int16;
+BytomMatList16* matList_int16;
 uint8_t result[32] = {0};
-map <vector<uint8_t>, BytomMatList16> seedCache;
+map <vector<uint8_t>, BytomMatList16*> seedCache;
 
 uint8_t *SimdTs(uint8_t blockheader[32], uint8_t seed[32]){
     vector<uint8_t> seedVec(seed, seed + 32);
 
     if(seedCache.find(seedVec) != seedCache.end()) {
-        printf("%s\n", "Already existed!");
+        printf("\t%s\n", "Seed already exists in the cache.");
         matList_int16 = seedCache[seedVec];
     } else {
         uint32_t exted[32];
@@ -23,11 +23,10 @@ uint8_t *SimdTs(uint8_t blockheader[32], uint8_t seed[32]){
         Words32 extSeed;
         init_seed(extSeed, exted);
 
-        // matList_int16 = new BytomMatList16;
-        // matList_int16->init(extSeed);
-        matList_int16.init(extSeed);
+        matList_int16 = new BytomMatList16;
+        matList_int16->init(extSeed);
 
-        seedCache.insert(pair<vector<uint8_t>, BytomMatList16>(seedVec, matList_int16));
+        seedCache.insert(pair<vector<uint8_t>, BytomMatList16*>(seedVec, matList_int16));
     }
 
     iter_mineBytom(blockheader, 32, result);
