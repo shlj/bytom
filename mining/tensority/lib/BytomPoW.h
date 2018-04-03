2b0d9b22cdd5854c198fea4fc80dc40c594df359
HAOYUatHZ <haoyu@protonmail.com>
2018-04-03 21:21:19 +0800

    fix timer

--- mining/tensority/lib/BytomPoW.h
+++ mining/tensority/lib/BytomPoW.h
@@ -389,10 +389,16 @@ static inline void iter_mineBytom(
     res8->add(*res8, resArr8[2]);
     res8->add(*res8, resArr8[3]);
 
-    end = clock();
-    // std::cout << "\tTime for getting MulMatix: "
-    //           << (double)(end - start) / CLOCKS_PER_SEC << "s"
-    //           << std::endl;
+    end = clock();    
+// #ifdef _USE_OPENMP
+//     std::cout << "\tTime for getting MulMatix: "
+//               << (double)(end - start) / CLOCKS_PER_SEC / 4 * 1000 << "ms"
+//               << std::endl;
+// #else
+//     std::cout << "\tTime for getting MulMatix: "
+//               << (double)(end - start) / CLOCKS_PER_SEC * 1000 << "ms"
+//               << std::endl;
+// #endif
 
     Arr256x64i32 arr(*res8);
     arr.reduceFNV();
